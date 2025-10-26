08943c3d42e4be26e8c3a84beefd52f72599da1b
Ohffs <ohffsnotnow@gmail.com>
2025-10-26 16:02:06 +0000

    Faff

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Team extends Model
 {
@@ -27,6 +28,11 @@ class Team extends Model
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'personal_team_id', 'id');
     }
 
     public function scheduledTasks(): HasMany
